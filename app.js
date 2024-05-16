9d0e60fde951fc003a45ba721e184e2d1450149a
Jazmín-Díaz <natividadjazminp@gmail.com>
2024-05-16 13:46:03 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const cors=require("cors");
+
+const app=express();
+
+//Rutas
+const AuthRoutes=require("./routers/Auth");
+const UserRoutes=require("./routers/Usuarios");
+
+//Middlewares para el funcionamiento
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+//Uploads
+app.use(express.static('uploads'));
+
+//configurar las rutas
+app.use("/api",AuthRoutes);
+app.use("/api",UserRoutes);
+//app.use("/api",TablasRutes);
+
+
+module.exports=app;
---
+++ constantes.js
@@ -0,0 +1,18 @@
+const DB_USER="natividad";
+const DB_PASSWORD="yulitza01";
+const DB_HOST="forweb.cpynjpn.mongodb.net";
+const DB_NAME="FotWeb";
+
+//const IP_SERVER="187.154.139.32";
+const IP_SERVER="127.0.0.1";
+
+JWT_SECRET_KEY="palabrasecreta123";
+
+module.exports={
+    DB_HOST,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USER,
+    IP_SERVER,
+    JWT_SECRET_KEY
+}
